8b654bcef7a5233e2215f7c2929e1af947cae467
Vipin <vrai8840@gmail.com>
2023-03-26 18:58:41 +0530

    added basic functionality of step counter

--- app/src/main/java/com/example/fitelevate/Activity_fragment.java
+++ app/src/main/java/com/example/fitelevate/Activity_fragment.java
@@ -1,17 +1,27 @@
 package com.example.fitelevate;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 
 public class Activity_fragment extends Fragment{
+    //chenged here
+    private boolean permissionAlreadyGranted = false;
+    private boolean permissionAlreadyDenied = false;
+    //yaha tak
+
+    String[] permissions = {"android.permission.ACTIVITY_RECOGNITION"};
 
     public CardView card1, card2, card3, card4, card5, card6;
     @Override
@@ -57,6 +67,9 @@ public class Activity_fragment extends Fragment{
         card5.setOnClickListener(view14 -> {
             Intent intent = new Intent(getActivity(), step_counter.class);
             startActivity(intent);
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                requestPermissions(permissions, 80);
+            }
         });
         //water intent
         card6.setOnClickListener(view15 -> {
@@ -65,4 +78,31 @@ public class Activity_fragment extends Fragment{
         });
         return view;
     }
+
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//
+//        if (requestCode == 80) {
+//            boolean permissionGranted = true;
+//            for (int result : grantResults) {
+//                if (result != PackageManager.PERMISSION_GRANTED) {
+//                    permissionGranted = false;
+//                    break;
+//                }
+//            }
+//            if (permissionGranted) {
+//                if (!permissionAlreadyGranted) {
+////                    Toast.makeText(getActivity(), "Permission Granted", Toast.LENGTH_SHORT).show();
+//                    permissionAlreadyGranted = true;
+//                }
+//            } else {
+//                if (!permissionAlreadyDenied) {
+//                    Toast.makeText(getActivity(), "Permission Denied", Toast.LENGTH_SHORT).show();
+//                    permissionAlreadyDenied = true;
+//                }
+//            }
+//        }
+//    }
+
 }
--- app/src/main/java/com/example/fitelevate/step_counter.java
+++ app/src/main/java/com/example/fitelevate/step_counter.java
@@ -1,14 +1,84 @@
 package com.example.fitelevate;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.TextView;
 
-public class step_counter extends AppCompatActivity {
+public class step_counter extends AppCompatActivity implements SensorEventListener {
+    private TextView textViewStepCounter, textViewStepDetector;
+    private SensorManager sensorManager;
+    private Sensor mStepCounter, mStepDetector;
+    private boolean isCounterSensorPresent, isDetectorSensorPresent;
+    int stepCount=0,stepDetect=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_step_counter);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        textViewStepCounter = findViewById(R.id.textViewStepCounter);
+        textViewStepDetector = findViewById(R.id.textViewStepDetector);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!=null){
+            mStepCounter = sensorManager.getDefaultSensor((Sensor.TYPE_STEP_COUNTER));
+            isCounterSensorPresent = true;
+        }
+        else {
+            textViewStepCounter.setText("Counter Sensor Is Not Present");
+            isCounterSensorPresent = false;
+        }
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR)!=null){
+            mStepDetector=sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+            isDetectorSensorPresent=true;
+        }
+        else{
+            textViewStepDetector.setText("Detector Sensor Is Not Present");
+            isDetectorSensorPresent=false;
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if(sensorEvent.sensor == mStepCounter){
+            stepCount = (int) sensorEvent.values[0];
+            textViewStepCounter.setText(String.valueOf(stepCount));
+        }else if(sensorEvent.sensor==mStepDetector){
+            stepDetect = (int) (stepDetect+sensorEvent.values[0]);
+            textViewStepDetector.setText(String.valueOf(stepDetect));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!=null)
+            sensorManager.registerListener(this, mStepCounter, SensorManager.SENSOR_DELAY_NORMAL);
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR)!=null)
+            sensorManager.registerListener(this, mStepDetector, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!=null)
+            sensorManager.unregisterListener(this, mStepCounter);
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR)!=null)
+            sensorManager.unregisterListener(this, mStepDetector);
     }
 }
