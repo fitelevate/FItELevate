d7d6eb367f7e64e7f6d83fa89874b966342bb524
Ajay <knitfitelevate@gmail.com>
2023-03-26 18:57:59 +0530

    exercise functionality added

---
+++ app/src/main/java/com/example/fitelevate/weightLoose.java
@@ -0,0 +1,14 @@
+package com.example.fitelevate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class weightLoose extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weight_loose);
+    }
+}
