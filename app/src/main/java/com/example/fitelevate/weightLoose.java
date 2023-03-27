15d662f67bb208a74533696969d3d8421a3bd2aa
Ajay <knitfitelevate@gmail.com>
2023-03-27 22:53:28 +0530

    yoga module added

--- app/src/main/java/com/example/fitelevate/weightLoose.java
+++ app/src/main/java/com/example/fitelevate/weightLoose.java
@@ -1,14 +1,40 @@
 package com.example.fitelevate;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class weightLoose extends AppCompatActivity {
+
+
+    int[] newArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weight_loose);
+
+        Toolbar toolbar= findViewById(R.id.toolbaar);
+        setSupportActionBar(toolbar);
+        newArray=new int[]{
+                //sabhi exercise ki id find karni hai according to their respective xml
+          R.id.pose1,R.id.pose2,
+        };
+    }
+    public void Imagebuttonclicked(View view) {
+        for (int i=0;i<newArray.length;i++){
+            if(view.getId()==newArray[i]){
+                int value=i+1;
+                Log.i("First",String.valueOf(value));
+                Intent intent=new Intent(weightLoose.this, ActivityWeightLooseExercise.class);
+                intent.putExtra("value",String.valueOf(value));
+                startActivity(intent);
+            }
+        }
+
     }
 }
