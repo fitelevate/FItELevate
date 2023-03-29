3b969ddebe0c52428db1b94125ff86695b893178
aman <amansingh672000@outlook.com>
2023-03-29 12:12:30 +0530

    added bmi

--- app/src/main/java/com/example/fitelevate/bmi.java
+++ app/src/main/java/com/example/fitelevate/bmi.java
@@ -2,13 +2,39 @@ package com.example.fitelevate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class bmi extends AppCompatActivity {
 
+    ImageView bmi_banner;
+    EditText bmi_weight_input, bmi_height_input;
+    Button calculate_bmi_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bmi);
+
+        bmi_banner = findViewById(R.id.bmi_banner);
+        bmi_weight_input = findViewById(R.id.bmi_weight_input);
+        bmi_height_input = findViewById(R.id.bmi_height_input);
+        calculate_bmi_button = findViewById(R.id.calculate_bmi_button);
+
+        calculate_bmi_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                float user_height = Float.parseFloat(String.valueOf(bmi_height_input.getText()))/100;
+                float user_weight = Float.parseFloat(String.valueOf(bmi_weight_input.getText()));
+                float bmi = user_weight / (user_height * user_height);
+                Intent i = new Intent(getApplicationContext(), bmi_result.class);
+                i.putExtra("bmi_result", bmi);
+                startActivity(i);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/fitelevate/bmi_result.java
@@ -0,0 +1,23 @@
+package com.example.fitelevate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class bmi_result extends AppCompatActivity {
+    TextView result_text, result_number;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bmi_result);
+
+        result_text = findViewById(R.id.result_text);
+        result_number = findViewById(R.id.result_number);
+
+        Intent i = getIntent();
+        float new_bmi = i.getFloatExtra("bmi_result", 0);
+        result_number.setText(String.valueOf(new_bmi));
+    }
+}
