1916641f37494039ea1bf1e5bebf42583ceb25af
Vipin <vrai8840@gmail.com>
2023-04-09 20:20:59 +0530

    minor bmi bmr fixes

--- app/src/main/java/com/example/fitelevate/bmr.java
+++ app/src/main/java/com/example/fitelevate/bmr.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -21,13 +22,9 @@ public class bmr extends AppCompatActivity {
     EditText bmr_height;
     EditText bmr_weight;
     EditText bmr_age;
-
     FirebaseUser user= FirebaseAuth.getInstance().getCurrentUser();
-
-
-    RadioButton maleRadioButton,femaleRadioButton;
-    boolean maleChecked = false;
-    boolean femaleChecked = false;
+    RadioGroup radioGroup;
+    RadioButton selectedGender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,21 +34,22 @@ public class bmr extends AppCompatActivity {
         bmr_height=findViewById(R.id.bmr_heightIn);
         bmr_weight=findViewById(R.id.weight_in);
         bmr_age=findViewById(R.id.age_input);
-        maleRadioButton=findViewById(R.id.male_radio_btn);
-        femaleRadioButton=findViewById(R.id.female_radio_btn);
+        radioGroup=findViewById(R.id.radiogroup);
         Button submitButton = findViewById(R.id.bmr_submit_btn);
         submitButton.setOnClickListener(view -> {
             String weightStr = bmr_weight.getText().toString();
             String heightStr = bmr_height.getText().toString();
             String ageStr = bmr_age.getText().toString();
-            if (!TextUtils.isEmpty(weightStr) && !TextUtils.isEmpty(heightStr) && !TextUtils.isEmpty(ageStr)) {
+            if (!TextUtils.isEmpty(weightStr) && !TextUtils.isEmpty(heightStr) && !TextUtils.isEmpty(ageStr) && radioGroup.getCheckedRadioButtonId()!=-1) {
                 float weight = Float.parseFloat(weightStr);
                 float height = Float.parseFloat(heightStr);
                 float age = Float.parseFloat(ageStr);
-                if (maleChecked) {
+                selectedGender = findViewById(radioGroup.getCheckedRadioButtonId());
+                String gender = selectedGender.getText().toString();
+                if (gender.equals("Male")) {
                     // Perform male BMR calculation
                     bmr = 88.362f + (13.397f * weight) + (4.799f * height) - (5.677f * age);
-                } else if (femaleChecked) {
+                } else {
                     // Perform female BMR calculation
                     bmr = 447.593f + (9.247f * weight) + (3.098f * height) - (4.330f * age);
                 }
@@ -73,33 +71,6 @@ public class bmr extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "Please fill in all fields", Toast.LENGTH_SHORT).show();
             }
         });
-    }
-
-    public void onRadioButtonClicked(View view) {
-        // Is the button now checked?
-        boolean checked = ((RadioButton) view).isChecked();
-
-        // Check which radio button was clicked
-        switch(view.getId()) {
-            case R.id.male_radio_btn:
-                if (checked) {
-                    maleChecked = true;
-                    femaleChecked = false;
-                    bmr_height.setText("");
-                    bmr_weight.setText("");
-                    bmr_age.setText("");
-                }
-                break;
-            case R.id.female_radio_btn:
-                if (checked) {
-                    maleChecked = false;
-                    femaleChecked = true;
-                    bmr_height.setText("");
-                    bmr_weight.setText("");
-                    bmr_age.setText("");
-                }
-                break;
-        }
     }
 }
 
